32147dd75f7602b25c875b78a882740b47c7adb5
mahilumphil <mahilum.philipp@yahoo.com>
2016-12-21 17:53:46 +0800

    Beginning

--- SystemProject/App_Start/BundleConfig.cs
+++ SystemProject/App_Start/BundleConfig.cs
@@ -12,7 +12,8 @@ namespace SystemProject
                         "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+                        "~/Scripts/jquery.validate*"
+                        ));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
@@ -21,10 +22,15 @@ namespace SystemProject
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
+                      "~/Scripts/bootstrap.min.js",
+                      "~/Scripts/jquery.js",
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                      "~/Content/bootstrap.min.css",
+                      "~/Content/bootstrap-theme.css",
+                      "~/Content/bootstrap-theme.min.css",
                       "~/Content/site.css"));
         }
     }
