51b11ea54eedd606960b010113c0e6f3264ed08f
mahilumphil <mahilum.philipp@yahoo.com>
2016-12-29 15:53:37 +0800

    modified

---
+++ SystemProject/Controllers/RegistrationController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Microsoft.AspNet.Identity;
+
+namespace SystemProject.Controllers
+{
+    public class RegistrationController : ApiController
+    {
+        private Data.projectdbDataContext db = new Data.projectdbDataContext();
+
+        [Authorize]
+        [Route]
+        [Route/"api/registration/list"]
+
+    }
+}
+
