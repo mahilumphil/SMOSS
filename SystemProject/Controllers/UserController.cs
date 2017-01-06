7fc92ba24379ff20ad4f41daac9edeec76a71e97
mahilumphil <mahilum.philipp@yahoo.com>
2017-01-06 10:41:19 +0800

    sad

---
+++ SystemProject/Controllers/UserController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SystemProject.Models;
+
+namespace SystemProject.Controllers
+{
+    public class UserController : Controller
+    {
+        protected override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            if (User != null)
+            {
+                var context = new ApplicationDbContext();
+                var username = User.Identity.Name;
+
+                if (!string.IsNullOrEmpty(username))
+                {
+                    var user = context.Users.SingleOrDefault(u => u.UserName == username);
+                    string fullName = string.Concat(new string[] { user.FullName });
+
+                    ViewData.Add("UserName", User.Identity.Name);
+                }
+            }
+            base.OnActionExecuted(filterContext);
+        }
+
+        public UserController()
+        { }
+    }
+}
