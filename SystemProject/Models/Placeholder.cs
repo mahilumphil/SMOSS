3afb0cfa9fd657a979d56c3b9a2c1cd37347bbe2
mahilumphil <mahilum.philipp@yahoo.com>
2017-01-07 16:36:14 +0800

    sad

---
+++ SystemProject/Models/Placeholder.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+
+namespace SystemProject.Models
+{
+    class PlaceholderAttribute : Attribute
+    {
+        public class PlaceHolderAttribute : Attribute, IMetadataAware
+        {
+            private readonly string _placeholder;
+            public PlaceHolderAttribute(string placeholder)
+            {
+                _placeholder = placeholder;
+            }
+
+            public void OnMetadataCreated(ModelMetadata metadata)
+            {
+                metadata.AdditionalValues["placeholder"] = _placeholder;
+            }
+        }
+    }
+}
